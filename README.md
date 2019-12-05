8205763a3bee1caf20ac4f26e9efe7d9770b9c19
popmac <parade8823@gmail.com>
2019-12-05 22:04:35 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## 利用手順
+- `app/batches` などにファイルを置く
+- rails cでコンソールに入って `OutputActionNames.run` を実行
+- 出力されたCSVファイルをスプレッドシートにインポート
---
+++ output_action_names.rb
@@ -0,0 +1,41 @@
+class OutputActionNames
+  def self.run
+    require 'fileutils'
+    require 'csv'
+    require 'active_support'
+    require 'active_support/inflector'
+
+    # 対象となるcontrollerのファイルを取得(ディレクトリ自体やconcernsを除外)
+    controller_files = Dir.glob('app/controllers/**/*').sort
+    target_controller_files = controller_files.grep(/.*_controller\.rb/)
+
+    # controllerのクラス名を取得
+    class_names = target_controller_files.map do |file|
+      file
+        .sub(/^app\/controllers\//, '')
+        .sub(/\.rb/, '')
+        .camelize
+    end
+
+    # 以下のような構造にする
+    # [['MembersController', 'index'], ['MembersController', 'new']]
+    class_name_and_action_array = []
+    class_names.each do |class_name|
+      actions = eval("#{class_name}.instance_methods(false).sort")
+
+      actions.each do |action|
+        class_name_and_action_array << [class_name, action.to_s]
+      end
+    end
+
+    # CSVで出力
+    # ['MembersController', 'index']をfに入れていることで、スプレッドシートにインポートする時にカンマ区切りになって便利
+    CSV.open('tmp/output.csv', 'w') do |f|
+      class_name_and_action_array.each do |a|
+        f << a
+      end
+    end
+
+    puts 'CSVファイルが tmp/output.csv に出力されました'
+  end
+end
